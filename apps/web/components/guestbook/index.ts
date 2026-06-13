a8c9d472b4ecb52538e7cc5e1546f15dc489fb5d
yevpt <vpt940417@gmail.com>
2026-06-13 15:39:31 +0800

    refactor(guestbook): 删除 GuestbookReplies，统一使用 CommentReplies
    - 删除 guestbook-replies.tsx 和 guestbook-replies.test.tsx
    - 从 index.ts 移除对应 re-export

--- apps/web/components/guestbook/index.ts
+++ apps/web/components/guestbook/index.ts
@@ -1,6 +1,5 @@
 export { GuestbookPage } from "./guestbook-page";
 export { GuestbookList } from "./guestbook-list";
 export { GuestbookItem } from "./guestbook-item";
-export { GuestbookReplies } from "./guestbook-replies";
 export { GuestbookInputBar } from "./guestbook-input-bar";
 export type { ReplyTarget } from "@/components/comments/comment-replies";
