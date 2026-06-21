ea62cf64f26667fb33521d74d0f06e590f0aaed6
yevpt <vpt940417@gmail.com>
2026-06-21 16:59:44 +0800

    feat(web): 导出 SidebarSectionFooter 与 SidebarFooterButton

--- apps/web/components/sidebar/index.ts
+++ apps/web/components/sidebar/index.ts
@@ -2,3 +2,4 @@ export { RecentVisitors } from "./recent-visitors";
 export { TagsCloud } from "./tags-cloud";
 export { SidebarSectionHeader } from "./sidebar-section-header";
 export { SidebarSectionAction } from "./sidebar-section-action";
+export { SidebarSectionFooter, SidebarFooterButton } from "./sidebar-section-footer";
